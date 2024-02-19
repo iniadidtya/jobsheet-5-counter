178121b76d9aaeda3679062d397e93ca634c7118
adidtya <pvtyanz@gmail.com>
2024-02-19 08:32:21 +0700

    menambahkan perubahan di routes-web.php

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 //route resource
 Route::resource('/posts', controller: \App\Http\Controllers\PostController::class);
-
+Route::get(‘/tambah’, [TambahController::class, ‘tambah’ ]);
 
